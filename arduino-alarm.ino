29dc50b165c82282e023712e7232d0ac8eed41c8
PowerBuxoK <fffdghffghff@gmail.com>
2025-11-05 17:34:33 +0700

    Fix screen thingamabob and add menu and lists(not yet done fully)

--- arduino-alarm.ino
+++ arduino-alarm.ino
@@ -12,8 +12,9 @@
 #define CHAR_WIDTH    9
 #define CHAR_HEIGHT   12
 #define CHAR_MARGIN   2
+#define ENTRY_HEIGHT  CHAR_HEIGHT + CHAR_MARGIN * 2
 
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R0, U8X8_PIN_NONE);
 
 // Variables are first 4 bytes, so its easy to empty just by casting a pointer to uint32_t and
 // setting it to 0, like a free optimization
@@ -31,19 +32,18 @@ struct EncoderInput
 
 struct MenuState
 {
-  uint8_t cur_menu = 0;
-  uint8_t cur_line = 0;
+  uint8_t cur_menu = 1;
+  uint8_t cur_line = -1;
+  bool locked      = false;
 };
 
-#define upd_timeout 10000
-int upd_timer       = 0;
+constexpr uint16_t upd_timeout = 10000;
+uint16_t upd_timer             = 0;
 
-bool screen_updated = false;
-bool update_screen  = false;
+bool screen_updated            = false;
+bool update_screen             = false;
 
-bool change_time    = false;
-
-long time           = 3660;
+long time                      = 3660;
 unsigned long long previousMillis;
 
 struct EncoderInput encoderInput;
@@ -110,18 +110,98 @@ void handleEncoder(struct EncoderInput& write_to)
 #define MENU_ENTRY_UNKNOWN 0x0
 #define MENU_ENTRY_NUMBER  0x1
 
+typedef uint8_t MenuEntryType;
 class Menu;
-class BasicMenuEntry;
+class BasicMenuEntry
+{
+public:
+  BasicMenuEntry() : m_type(0) {}
+  void Draw(uint8_t ypos, uint8_t color) {
+    // oled.setDrawColor(color);
+    // oled.drawBox(0, ypos, 128, ENTRY_HEIGHT);
+  };
+  MenuEntryType m_type = MENU_ENTRY_UNKNOWN;
+};
+
+class MenuEntryNumber : public BasicMenuEntry
+{
+public:
+  MenuEntryNumber(uint8_t data_count, const char* name)
+      : BasicMenuEntry(), m_data_count(data_count), m_name(name)
+  {
+    m_type = 1;
+    m_data = (int*)malloc(sizeof(int) * data_count);
+    for(uint8_t i = 0; i < data_count; i++)
+    {
+      m_data[i] = 0;
+    }
+  }
+  void Draw(uint8_t ypos, uint8_t color)
+  {
+    oled.setDrawColor(color);
+    oled.drawBox(0, ypos, 128, ENTRY_HEIGHT);
+    oled.setDrawColor(255 - color);
+    oled.drawStr(16, ypos + CHAR_MARGIN + CHAR_HEIGHT, m_name);
+  };
+  uint8_t m_data_count = 0;
+  const char* m_name;
+  int* m_data;
+};
 
 class Menu
 {
 public:
   Menu(const char* name, uint8_t entry_count) : m_name(name), m_entry_count(entry_count)
   {
-    m_entries  = (BasicMenuEntry*)malloc(sizeof(BasicMenuEntry*)); // Allocate space for pointers
+    m_entries  = (BasicMenuEntry*)malloc(sizeof(BasicMenuEntry*)
+                                         * m_entry_count); // Allocate space for pointers
     m_name_len = strlen(m_name);
   }
-  void handleInput(EncoderInput& input, MenuState& state) {}
+  void handleInput(EncoderInput& input, MenuState& state)
+  {
+    if(!state.locked)
+    {
+      if(encoderInput.turnedRight)
+      {
+        menuState.cur_line++;
+      }
+      else if(encoderInput.turnedLeft)
+      {
+        menuState.cur_line--;
+      }
+      if(menuState.cur_line != -1 && menuState.cur_line > m_entry_count)
+      {
+        menuState.cur_line = -1;
+      }
+    }
+  }
+  void drawEntries()
+  {
+
+    if(m_entries == nullptr)
+    {
+      oled.setDrawColor(255);
+      oled.drawStr(16, 64, "No entr");
+      return;
+    }
+    for(uint8_t i = 0; i < m_entry_count; i++)
+    {
+      BasicMenuEntry* entry    = (m_entries + i);
+      const MenuEntryType type = entry->m_type;
+      char c[2]                = "0";
+      c[0] += type;
+      oled.setDrawColor(255);
+      oled.drawStr(16, 64, c);
+      switch(type)
+      {
+        case MENU_ENTRY_NUMBER:
+          ((MenuEntryNumber*)entry)->Draw(i * ENTRY_HEIGHT, 255);
+          break;
+        default:
+          entry->Draw(i * ENTRY_HEIGHT, 255);
+      }
+    }
+  }
   const char* m_name        = "null";
   uint8_t m_name_len        = 0;
   BasicMenuEntry* m_entries = nullptr; // We dont know types of these entries, we can later cast
@@ -129,73 +209,43 @@ public:
   uint8_t m_entry_count     = 0;
 };
 
-class BasicMenuEntry
-{
-public:
-  BasicMenuEntry() : m_type(0) {}
-  uint8_t m_type = 0;
-};
-
-class MenuEntryNumber : public BasicMenuEntry
-{
-public:
-  MenuEntryNumber(uint8_t data_count, int defaults[]) : m_data_count(data_count)
-  {
-    m_data = (int*)malloc(sizeof(int) * data_count);
-    for(uint8_t i = 0; i < data_count; i++)
-    {
-      m_data[i] = defaults[i];
-    }
-  }
-  uint8_t m_data_count = 0;
-  int* m_data;
-};
-
 constexpr uint8_t menu_count = 2;
 Menu* menus                  = (Menu*)malloc(sizeof(Menu*) * menu_count);
 
-void drawTextBox(uint8_t x, uint8_t y, uint8_t color) {
-} // TODO: ADD THIS AND MAKE drawHead use this
-
+void drawTextBox(const uint8_t x, const uint8_t y, const uint8_t color, const char* text,
+                 const uint8_t text_length)
+{
+  oled.setDrawColor(255 - color);
+  oled.drawBox(x, y, text_length * CHAR_WIDTH + CHAR_MARGIN * 2, CHAR_HEIGHT + CHAR_MARGIN * 2);
+  oled.setDrawColor(color);
+  oled.drawStr(x + CHAR_MARGIN, y + CHAR_MARGIN + CHAR_HEIGHT, text);
+}
 void drawHead()
 {
   uint16_t x                     = 0;
   constexpr uint16_t text_height = CHAR_HEIGHT + CHAR_MARGIN;
-  u8g2.setDrawColor(0);
-  u8g2.drawBox(0, 0, SCR_WIDTH, text_height + CHAR_MARGIN);
+  oled.setDrawColor(0);
+  oled.drawBox(0, 0, SCR_WIDTH, text_height + CHAR_MARGIN);
 
   for(uint8_t i = 0; i < menu_count; i++)
   {
     const char* name       = menus[i].m_name;
     const uint8_t name_len = menus[i].m_name_len;
 
-    if(menuState.cur_menu == i)
-    {
-      u8g2.setDrawColor(255);
-      u8g2.drawBox(x, 0, name_len * CHAR_WIDTH + CHAR_MARGIN * 2, CHAR_HEIGHT + CHAR_MARGIN * 2);
-      u8g2.setDrawColor(0);
-      u8g2.drawStr(x + CHAR_MARGIN, text_height, name);
-    }
-    else
-    {
-      u8g2.setDrawColor(0);
-      u8g2.drawBox(x, 0, name_len * CHAR_WIDTH + CHAR_MARGIN * 2, CHAR_HEIGHT + CHAR_MARGIN * 2);
-      u8g2.setDrawColor(255);
-      u8g2.drawStr(x + CHAR_MARGIN, text_height, name);
-    }
-    u8g2.setDrawColor(255);
-    u8g2.drawLine(0, text_height + CHAR_MARGIN, SCR_WIDTH, text_height + CHAR_MARGIN);
+    drawTextBox(x, 0, menuState.cur_menu == i ? 0 : 255, name, name_len);
+    oled.setDrawColor(255);
+    oled.drawLine(0, text_height + CHAR_MARGIN, SCR_WIDTH, text_height + CHAR_MARGIN);
     x += (name_len * CHAR_WIDTH) + CHAR_MARGIN * 2;
   }
 }
 
-void drawMainMenu()
+void drawMainMenu(class Menu& cur_menu)
 {
   { // Time draw scope
-    u8g2.setFont(u8g2_font_profont29_tr);
-    u8g2.setDrawColor(255);
-    u8g2.drawBox(0, 12, 24 * 4, 32);
-    u8g2.setDrawColor(0);
+    oled.setFont(u8g2_font_profont29_tr);
+    oled.setDrawColor(255);
+    oled.drawBox(0, 12, 24 * 4, 32);
+    oled.setDrawColor(0);
     constexpr uint32_t secs_day = 86400;
     constexpr uint32_t secs_hrs = 3600;
     constexpr uint32_t secs_min = 60;
@@ -209,27 +259,28 @@ void drawMainMenu()
     to_prt[4]                   = (mins % 10) + '0';
     to_prt[3]                   = (mins / 10 % 10) + '0';
 
-    u8g2.drawStr(6, 34, to_prt);
+    oled.drawStr(6, 34, to_prt);
 
-    u8g2.setFont(u8g2_font_profont17_mf);
+    oled.setFont(u8g2_font_profont17_mf);
   }
   { // Icons draw scope
   }
 }
 
-void drawSetTime() {}
+void drawSetTime(class Menu& cur_menu) { cur_menu.drawEntries(); }
 
 void dispUpdate()
 {
-  u8g2.clearBuffer();
+  oled.clearBuffer();
+  Menu& curMenu = menus[menuState.cur_menu];
 
   switch(menuState.cur_menu)
   {
     case 0:
-      drawMainMenu();
+      drawMainMenu(curMenu);
       break;
     case 1:
-      drawSetTime();
+      drawSetTime(curMenu);
       break;
     default:
       menuState.cur_menu = 0;
@@ -240,25 +291,40 @@ void dispUpdate()
     drawHead();
   }
 
-  u8g2.sendBuffer();
+  oled.sendBuffer();
 }
 
 void handleMenus()
 {
-
-  if(encoderInput.keyHeld)
+  switch(menuState.cur_menu)
   {
-    if(encoderInput.turnedRight)
+    case 0:
+      break;
+    case 1:
+      break;
+    default:
+      menuState.cur_menu = 0;
+  }
+
+  if(menuState.locked)
+  {
+  }
+  else
+  {
+    if(encoderInput.keyHeld)
     {
-      menuState.cur_menu++;
-      menuState.cur_line = 0;
+      if(encoderInput.turnedRight)
+      {
+        menuState.cur_menu++;
+        menuState.cur_line = -1;
+      }
+      else if(encoderInput.turnedLeft)
+      {
+        menuState.cur_menu--;
+        menuState.cur_line = -1;
+      }
+      menuState.cur_menu %= menu_count;
     }
-    else if(encoderInput.turnedLeft)
-    {
-      menuState.cur_menu--;
-      menuState.cur_line = 0;
-    }
-    menuState.cur_menu %= menu_count;
   }
 
   Menu& curMenu = menus[menuState.cur_menu];
@@ -277,15 +343,15 @@ void setup()
   pinMode(ENC_BTN, INPUT);
   pinMode(BEEPER, OUTPUT);
 
-  u8g2.begin();
-  u8g2.setFont(u8g2_font_profont17_mf);
-
   // Build menus
-  int zerofill[]        = { 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // A bit cringe but why the fuck not
-  menus[0]              = Menu("MAIN", 0);
-  menus[1]              = Menu("ST", 0);
-  menus[1].m_entries[0] = MenuEntryNumber(2, zerofill); // Time
-  menus[1].m_entries[1] = MenuEntryNumber(3, zerofill); // Date
+  // int zerofill[]        = { 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // A bit cringe but why the fuck not
+  // menus[0]              = Menu("MAIN", 0);
+  menus[1]              = Menu("ST", 1);
+  menus[1].m_entries[0] = MenuEntryNumber(2, ""); // MenuEntryNumber(2, "Time"); // Time
+  // menus[1].m_entries[1] = MenuEntryNumber(3, zerofill, "Date"); // Date
+
+  oled.begin();
+  oled.setFont(u8g2_font_profont17_mf);
 }
 
 void loop()
